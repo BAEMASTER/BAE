b20ff5b23429e9af1b0e95634daca7bc94efb2bb
BAEMASTER <jasrotman@gmail.com>
2026-01-18 10:29:05 -0800

    new match

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import './globals.css';
 import Header from '@/components/Header';
 import { ReactNode } from 'react';
@@ -8,25 +7,21 @@ import { usePathname } from 'next/navigation';
 export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const isAuthPage = pathname.startsWith('/auth');
-
+  const isMatchPage = pathname.startsWith('/match');
+  
   return (
     <html lang="en">
       <body className="min-h-screen">
-
-        {/* Header visible everywhere except auth */}
         {!isAuthPage && <Header />}
-
         <main
           className={
-            isAuthPage
-  ? "min-h-screen bg-gradient-to-br from-rose-100 via-fuchsia-100 to-indigo-100"
-  : "min-h-screen bg-gradient-to-br from-rose-100 via-fuchsia-100 to-indigo-100 pt-[72px]"
-
+            isAuthPage || isMatchPage
+              ? "min-h-screen bg-gradient-to-br from-rose-100 via-fuchsia-100 to-indigo-100"
+              : "min-h-screen bg-gradient-to-br from-rose-100 via-fuchsia-100 to-indigo-100 pt-[72px]"
           }
         >
           {children}
         </main>
-
       </body>
     </html>
   );
