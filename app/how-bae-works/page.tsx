fb1568d70652170b317b9cf2ae0440716187a591
BAEMASTER <jasrotman@gmail.com>
2026-01-22 22:31:25 -0800

    HBW

--- app/how-bae-works/page.tsx
+++ app/how-bae-works/page.tsx
@@ -337,7 +337,7 @@ export default function GuidePage() {
                   </div>
                 </div>
               </div>
-              <p className="text-xs text-white/50 text-center">Gold glowing = shared interests | Unique interests below each person</p>
+              <p className="text-xs text-white/50 text-center">Gold glowing = shared interests | See an interest you like? Tap to add it to your profile!</p>
             </div>
           </div>
         </motion.section>
